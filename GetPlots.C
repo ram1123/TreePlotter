847d862b98885e71194436024784558aae331b19
ram1123 <ramkrishna.sharma71@gmail.com>
2015-08-26 14:05:11 +0530

    Fix bug related to cut in GetPlots.C

--- GetPlots.C
+++ GetPlots.C
@@ -4,15 +4,21 @@
  */
 #include "TCanvas.h"
 #include "TROOT.h"
+#include <iostream>
 
 void GetPlots()
 {
 gROOT->ProcessLine(".L CompareAdvanced.C");
 
+const string Cut = "(nPV>10)";
+
 TCanvas *Canvas1 = new TCanvas("c1236","Muon",200,10,800,700);
-gROOT->ProcessLine("compareQuantities(1,0,1,\"otree\",\"nPV\",\"\",\"\",40,0,40,\"\",2,\"/tmp/WWTree_data_el_17jul.root\",\"DataEl\",\"/tmp/WWTree_WJets_50ns.root\",\"WJets\")");
-//gROOT->ProcessLine("compareQuantities(1,0,1,\"NVtx\",\"\",\"\",40,0,40,\"\",2,\"/home/ramkrishna/TEMP/Analysis/SingleMuonData2015B.root\",\"DataMu\",\"/home/ramkrishna/TEMP/Analysis/Merged_WJets.root\",\"WJets\")");
+
+
+gROOT->ProcessLine(Form("compareQuantities(1,0,1,\"otree\",\"nPV\",\"\",\"\",40,0,40,\"%s\",2,\"/tmp/WWTree_data_el_17jul.root\",\"DataEl\",\"/tmp/WWTree_WJets_50ns.root\",\"WJets\")",Cut.c_str()));
 Canvas1->SaveAs("test.pdf");
 Canvas1->Clear();
+
+// You have to clone last 3 line for each variable you want to plot.
 
 } 
