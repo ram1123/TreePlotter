b90dc45b8fb69f5edbd6c33c1dee5d7094d3f96b
ram1123 <ramkrishna.sharma71@gmail.com>
2016-03-03 12:51:03 +0530

    Cleaned MakeMasterReader.py

--- MakeMasterReader.py
+++ MakeMasterReader.py
@@ -57,10 +57,8 @@ print 'int NumPV;'
 
 print 'int j=0;\t//for bins\nint k=1;\t//reseting the color of backgrounds'
 ############ END:: Setting Bin, Min and Max       ###########################
-#for a in range(0,3):
-#    for b in range(0,3):
-#        print "test = ",a+b+a*2
-#
+
+
 ########## Define STACKED histogram  ########################
 print '\tTCanvas * c1 = new TCanvas("c1","",500,500);\n'
 print '\tTFile f("/tmp/PileUpHisto.root");'
@@ -68,27 +66,19 @@ print '\tTH1F *h = (TH1F*)f.Get("j1_1");'
 print '\tint binidx;'
 print '\tfloat Nvtx_weight;'
 print ''
-#print '\tTH1F *hPUWeight = new TH1F("hPUWeight","",40,0,5);'
 print "\tTHStack** hs = new THStack*[%i];"%(len(data.varList)/4)
 print "\tTPad** pad = new TPad*[%i];"%(len(data.varList)/4)
 print "\tTPad** padB = new TPad*[%i];"%(len(data.varList)/4)
-#print '\tTLegend *leg = new TLegend*[%i];'%(len(data.list_mc_sig)*len(data.varList)/4)
 print '\tTH1F**hRatio = new TH1F*[%i];'%(len(data.varList)/4)
 print "\tfor(int i=0; i<%i;i++){"%(len(data.varList)/4)
 print '\t\ths[i] = new THStack(Form("hs%i",i),"");'
 print '\t\tpad[i] = new TPad(Form("pad%i",i),"",0,0.30,1,1);'
 print '\t\tpadB[i] = new TPad(Form("padB%i",i),"",0,0,1,0.30);'
-#pirint '\t\tleg[i] = new TLegend(0.70,0.80,0.85,0.90,NULL,"brNDC")'
 print '\t\thRatio[i] = new TH1F(Form("hRatio%i",i),"Ratio",Bin[i],Min[i],Max[i]);'
-#print '\t\tpad[i]->SetBottomMargin(0.05287462);;'
-#print '\t\tpad[i]->SetLogy(1);'
-#print '\t\tpad[i]->Draw();'
-#print '\t\tpad[i]->cd();'
 print '\t}\n'
-#print '\t\tSigHist[i]->SetLineColor(1);'
-#print '\t\tSigHist[i]->SetFillColor(1);\n\t}\n'
-#print '\t\tSigHist[i]->SetLineColor(2i+1);\n\t}\n'
 ########## END:: Define STACKED histogram  ########################
+
+
 ########## Define histogram for sig mc  ########################
 for a in range(0,len(data.list_mc_sig)):
 	print "\tTH1F** t%i_SigHist = new TH1F*[%i];"%(a, len(data.varList)/4)
@@ -100,8 +90,6 @@ for a in range(0,len(data.list_mc_sig)):
 	print '\t\tt%i_SigHist[i]->SetMarkerColor(k);'%a
 	print '\t\tt%i_SigHist[i]->SetLineWidth(k);'%a
 	print '\t\tt%i_SigHist[i]->SetLineColor(k);\n\t}\n'%a
-#print '\t\tSigHist[i]->SetFillColor(1);\n\t}\n'
-#print '\t\tSigHist[i]->SetLineColor(2i+1);\n\t}\n'
 
 ########## Define histogram for bkg mc  ########################
 for a in range(0,len(data.list_mc_bkg)):
@@ -117,7 +105,6 @@ for a in range(0,len(data.list_mc_bkg)):
 	print '\t\tt%i_BkgHist[i]->SetLineColor(k);'%a
 	print '\t\tt%i_BkgHist[i]->SetMarkerColor(k);'%a
 	print '\t\tt%i_BkgHist[i]->SetFillColor(k);\n\t}\n'%a
-	#print '\t\tBkgHist[i]->SetLineColor(2*(2*(i+1)));\n\t}\n'
 
 ########## Define histogram for data mc  ########################
 print "\tTH1F** DataHist = new TH1F*[%i];"%(len(data.list_data)*len(data.varList)/4)
@@ -127,7 +114,6 @@ print '\t\tDataHist[i] = new TH1F(Form("DataHist%i",i),"",Bin[i],Min[i],Max[i]);
 print '\t\tDataHist[i]->SetLineColor(1);'
 print '\t\tDataHist[i]->SetMarkerColor(1);'
 print '\t\t//DataHist[i]->Sumw2();\n\t}\n'
-#print '\t\tDataHist[i]->SetLineColor(2*(2*i+1));\n\t}\n'
 
 ################################################################
 for a in range(0,len(data.list_mc_sig)):
@@ -146,8 +132,6 @@ for a in range(0,len(data.list_mc_sig)):
         	print '\t\t\tt%i_SigHist[%i]->Fill(mc_sig_%s.%s,Nvtx_weight);'%(a, b, a, data.varList[b*4])
 	else:
         	print '\t\t\tt%i_SigHist[%i]->Fill(mc_sig_%s.%s);'%(a, b, a, data.varList[b*4])
-        #print '\t\t\tt%i_SigHist[%i]->Fill(mc_sig_%s.%s);'%(a, a+b+a*((len(data.varList)/4)-1), a, data.varList[b*4])
-        #print '\t\t\tSigHist[%i]->Fill(mc_sig_0.%s,Nvtx_weight*100);'%(a+b+a*((len(data.varList)/4)-1), data.varList[b*4])
     print '\t\t}'
     print '\n'
 
@@ -162,12 +146,10 @@ for a in range(0,len(data.list_mc_bkg)):
     print '\t\t\tNvtx_weight = h->GetBinContent(binidx);'
     print '\t\t\tif(Nvtx_weight ==0) Nvtx_weight = 1 ;'
     for b in range(0, len(data.varList)/4):
-        #print '\t\t\tt%i_BkgHist[%i]->Fill(mc_bkg_%i.%s);'%(a,b,a, data.varList[b*4])
 	if data.ifPUCorr == 1:
         	print '\t\t\tt%i_BkgHist[%i]->Fill(mc_bkg_%i.%s,Nvtx_weight);'%(a,b,a, data.varList[b*4])
 	else:
         	print '\t\t\tt%i_BkgHist[%i]->Fill(mc_bkg_%i.%s);'%(a,b,a, data.varList[b*4])
-        #print '\t\t\tt%i_BkgHist[%i]->Fill(mc_bkg_%i.%s);'%(a,a+b+a*((len(data.varList)/4)-1),a, data.varList[b*4])
     print '\t\t}'
     print '\n'
 
@@ -182,17 +164,13 @@ for a in range(0,len(data.list_data)):
     print '\t\t}'
     print '\n'
 
-#print '\tTCanvas * c1 = new TCanvas("c1","",1);\n'
 print '\tTLegend *leg = new TLegend(0.79,0.40,1.09,0.89,NULL,"brNDC");\n'
 print 'double yMax=0.;\ndouble HistMax=0.0;\nc1->cd();\n'
-#for a in range(0,len(data.list_mc_sig)*len(data.varList)/4):
 for a in range(0,len(data.varList)/4):
     #print '\n\n\n\n===========================================================\n\n'
     #print a
     #print '\n\n\n\n===========================================================\n\n'
 
-    #print '\tSigHist[%i]->Scale(1/SigHist[%i]->Integral());'%(a, a)
-    #print 'pad[%i]->SetBottomMargin(0.0);'%a
     print 'pad[%i]->SetBottomMargin(0.05287462);'%a
     print 'pad[%i]->SetLogy(1);'%a
     print 'pad[%i]->Draw();'%a
@@ -210,49 +188,37 @@ for a in range(0,len(data.varList)/4):
 
     for bkg in range(0,len(data.list_mc_bkg)):
     	print '\tt%i_BkgHist[%i]->Scale(%f);'%(bkg,a,float(data.scale[bkg]))
-    	#print '\tBkgHist[%i]->Scale(1/BkgHist[%i]->Integral());'%(a, a)
     	print '\tHistMax = t%i_BkgHist[%i]->GetMaximum()*1.15;'%(bkg, a)
     	print '\tt0_SigHist[0]->SetMaximum(TMath::Max(HistMax,yMax));'
     	print '\tt0_SigHist[0]->SetMinimum(0.0);'
-    	#print '\tt0_SigHist[%i]->SetMaximum(TMath::Max(HistMax,yMax));'%(bkg)
     	print '\tleg->AddEntry(t%i_BkgHist[%i],"%s","f");'%(bkg, a, str(data.NameBkg[bkg]))
     	print '\tyMax=HistMax;\n'
 	print '\tcout<<"=====> \tHistMax = "<<yMax<<endl;'
 	print '\tcout<<"============================>>> \t MaxValue Y-axis = "<< t%i_SigHist[%i]->GetMaximum() << endl;'%(sig,a)
 
 
-    #print '\tDataHist[%i]->Scale(1/DataHist[%i]->Integral());'%(a, a)
     print '\tDataHist[%i]->Sumw2();'%a
     print '\tHistMax = DataHist[%i]->GetMaximum()*1.15;'%a
-    #print '\tt0_SigHist[%i]->SetMaximum(TMath::Max(HistMax,yMax));'%(a)
     print '\tleg->AddEntry(DataHist[%i],"Data","pe");'%(a)
     print '\tyMax=HistMax;\n'
-    #for sig in range(0,len(data.list_mc_sig)):
-    #	print '\ths[%i]->Add(t%i_SigHist[%i],"hist");'%(a, sig, a)
 
     for bkg in range(0,len(data.list_mc_bkg)):
     	print '\ths[%i]->Add(t%i_BkgHist[%i],"hist");'%(a, bkg, a)
-    #print '\ths[%i]->Add(BkgHist[%i]);'%(a,a+3)
     print '\ths[%i]->Draw();'%a
-    #    print '\ths%i->Add(BkgHist[%i]);'%a
-    #print '\tBkgHist[%i]->Draw("same");'%a
     print '\tDataHist[%i]->Draw("same");'%a
     for sig in range(0,len(data.list_mc_sig)):
     	print '\tt%i_SigHist[%i]->Draw("same");'%(sig,a)
     print '\tleg->Draw("same");'
 
-    #print 'TH1F *hRatio[%i] = new TH1F("hRatio[%i]","Ratio",40,0,40);'
     if a==0:
 	print 'TH1F *h2 = (TH1F*)DataHist[%i]->Clone();'%a
     else:
 	print 'h2 = (TH1F*)DataHist[%i]->Clone();'%a
     for bkg in range(0,len(data.list_mc_bkg)):
     	print 'hRatio[%i]->Add(t%i_BkgHist[%i]);'%(a, bkg, a)
-    #print 'hRatio[%i]->Add(BkgHist[%i]);'%(a,a+3)
     print ''
     print ''
     print 'c1->cd();'
-    #print 'TPad *padB[%i] = new TPad("padB[%i]","padB[%i]",0,0,1,0.30);'
     print 'padB[%i]->SetGridy(1);'%a
     print 'padB[%i]->SetTopMargin(0.0);'%a
     print 'padB[%i]->SetBottomMargin(0.30);'%a
@@ -271,14 +237,10 @@ for a in range(0,len(data.varList)/4):
     	print 'cout<<"Number of event in %s  = "<<t%i_BkgHist[%i]->Integral()<<endl;'%(data.NameSig[sig],sig,a)
     for bkg in range(0,len(data.list_mc_bkg)):
     	print 'cout<<"Number of event in %s  = "<<t%i_BkgHist[%i]->Integral()<<endl;'%(data.NameBkg[bkg],bkg,a)
-    	#print 'cout<<"Number of event in t%i_BkgHist[%i]  %s  = "<<t%i_BkgHist[%i]->Integral()<<endl;'%(bkg,a,data.list_mc_bkg[bkg],bkg,a)
 
-    #for a in range(0,len(data.list_mc_sig)):
-    #print '\t\tt%i_mc_sig->Add("%s%s");'%(a, data.store_mc_sig, data.list_mc_sig[a])
     print 'h2->SetMarkerStyle(21);'
     print 'h2->SetStats(0);'
     print 'h2->GetYaxis()->SetTitle("Data/#Sigma MC");'
-    #   print a
     print 'h2->GetXaxis()->SetTitle("%s");'%(str(data.varList[4*a]))
     print 'h2->SetMinimum(0.5);'
     print 'h2->SetMaximum(1.5);'
@@ -295,6 +257,5 @@ for a in range(0,len(data.varList)/4):
     print '\tc1->SaveAs("%s.png");'%data.varList[a*4]
     print '\tleg->Clear();'
     print '\tc1->Clear();\n\t//###########################################################\n\n'
-#    print 'Ramkrishna ',a
 
 print '\n\n}'
